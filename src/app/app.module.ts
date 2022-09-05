42e77eb362603f86071a91309106990a68dc0168
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-09-05 16:51:19 +0530

    Done Login Page Form Design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from "@angular/material/slider";
+
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import {MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule } from '@angular/material/button';
+import { LoginPageComponent } from './components/login-page/login-page.component'
+import { MatCardModule } from '@angular/material/card'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    LoginPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
