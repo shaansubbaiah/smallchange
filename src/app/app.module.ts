0a4da72432dfbbc061143420ae1d830e24180322
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-09-06 10:28:34 +0530

    Re-added login page component with navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
-
+import { LoginPageComponent } from "./components/login-page/login-page.component";
 import {MatFormFieldModule } from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
@@ -14,11 +14,14 @@ import {MatButtonModule } from '@angular/material/button';
 
 import { MatCardModule } from '@angular/material/card';
 
-import { NavbarComponent } from './components/navbar/navbar.component'
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginFormComponent } from './login-form/login-form.component'
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    LoginPageComponent,
+    LoginFormComponent
   ],
   imports: [
     BrowserModule,
