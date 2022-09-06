4ce19b1e8bb8ca544783fd29ba4d17e18223dd5a
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-09-06 22:55:37 +0530

    Cleanup imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,34 +9,16 @@ import { CoreModule } from './core/core.module';
 import { FeaturesModule } from './features/features.module';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-// import {MatToolbarModule} from '@angular/material/toolbar';
-
-// import { LoginPageComponent } from "./components/login-page/login-page.component";
-// import {MatFormFieldModule } from '@angular/material/form-field';
-// import {MatInputModule} from '@angular/material/input';
-// import {MatIconModule} from '@angular/material/icon';
-// import {MatButtonModule } from '@angular/material/button';
-
-// import { MatCardModule } from '@angular/material/card';
-
-// import { NavbarComponent } from './components/navbar/navbar.component';
-// import { LoginFormComponent } from './login-form/login-form.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     CoreModule,
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     SharedModule,
     FeaturesModule,
-    // BrowserAnimationsModule,
-    // MatFormFieldModule,
-    // MatInputModule,
-    // MatIconModule,
-    // MatButtonModule,
-    // MatCardModule,
-    // MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent],
