df134e7d8179fc4548d850602b41771358f66aba
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-09-13 23:22:47 +0530

    Updated login form component; Added login auth service

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,18 +4,18 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
+import { LoginFormComponent } from './login-page/login-form/login-form.component';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
-
+import { MatCardModule } from "@angular/material/card";
 import { LoginPageComponent } from './login-page/login-page.component';
-import { LoginFormComponent } from './login-page/login-form/login-form.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { TradeHistoryComponent } from './trade-history/trade-history.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { BuyTradeComponent } from './buy-trade/buy-trade.component';
 import { SellTradeComponent } from './sell-trade/sell-trade.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -31,7 +31,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   imports: [
     CommonModule,
     MatFormFieldModule,
+    ReactiveFormsModule,
     MatInputModule,
+    MatCardModule,
     MatIconModule,
     MatCardModule,
     MatButtonModule,
