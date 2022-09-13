1b48de360b21132a44e41ec98e02e1cfbdc6a258
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-09-13 21:46:56 +0530

    Add trade history table

--- src/app/core/models/trade-stock.ts
+++ src/app/core/models/trade-stock.ts
@@ -1,22 +1,10 @@
 export interface TradeStock {
-<<<<<<< HEAD
-    name: string;
-    code: string;
-    quantity: number;
-    price: number;
-    asset_class : string;
-    trade_type : string;
-    date: string; // YYYY/MM/DD
-    time: string; //24 hour clock- HH:MM
-}
-=======
   name: string;
   code: string;
   quantity: number;
   price: number;
-  asset_class : string;
-  trade_type : string;
+  asset_class: string;
+  trade_type: string;
   date: string; // YYYY/MM/DD
   time: string; //24 hour clock- HH:MM
 }
->>>>>>> cf3608a390b5b933bdd5e5a8583d6de409c4f437
--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { dummy_data_order } from '../../core/models/mock-data';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
   constructor() {}
+
+  getTradeHistory() {
+    return dummy_data_order;
+  }
 }
