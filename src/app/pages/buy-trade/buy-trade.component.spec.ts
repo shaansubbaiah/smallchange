c72d5c2ab4248c139abd016d704c03b2725b3611
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-09-13 17:47:02 +0530

    Create placeholder pages

---
+++ src/app/pages/buy-trade/buy-trade.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BuyTradeComponent } from './buy-trade.component';
+
+describe('BuyTradeComponent', () => {
+  let component: BuyTradeComponent;
+  let fixture: ComponentFixture<BuyTradeComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BuyTradeComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BuyTradeComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/trade-history/trade-history.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TradeHistoryComponent } from './trade-history.component';
+
+describe('TradeHistoryComponent', () => {
+  let component: TradeHistoryComponent;
+  let fixture: ComponentFixture<TradeHistoryComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TradeHistoryComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TradeHistoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
