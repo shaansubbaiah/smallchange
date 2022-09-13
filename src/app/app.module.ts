5b48eecca40b96f955a74ac3c4a0f58692a82f19
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-09-13 20:58:21 +0530

    Move pages components to features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,14 +9,8 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core/core.module';
 import { FeaturesModule } from './features/features.module';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BuyTradeComponent } from './pages/buy-trade/buy-trade.component';
-import { SellTradeComponent } from './pages/sell-trade/sell-trade.component';
-import { PortfolioComponent } from './pages/portfolio/portfolio.component';
-import { TradeHistoryComponent } from './pages/trade-history/trade-history.component';
-
 @NgModule({
-  declarations: [AppComponent, BuyTradeComponent, SellTradeComponent, PortfolioComponent, TradeHistoryComponent],
+  declarations: [AppComponent],
   imports: [
     CoreModule,
     BrowserModule,
--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -10,9 +10,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { LoginFormComponent } from './login-page/login-form/login-form.component';
 import { HomePageComponent } from './home-page/home-page.component';
+import { TradeHistoryComponent } from './trade-history/trade-history.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { BuyTradeComponent } from './buy-trade/buy-trade.component';
+import { SellTradeComponent } from './sell-trade/sell-trade.component';
 
 @NgModule({
-  declarations: [LoginPageComponent, LoginFormComponent, HomePageComponent],
+  declarations: [LoginPageComponent, LoginFormComponent, HomePageComponent, TradeHistoryComponent, PortfolioComponent, BuyTradeComponent, SellTradeComponent],
   imports: [
     CommonModule,
     MatFormFieldModule,
