407506499ab55fedb7e450a1f20bd96c248911e5
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-09-13 23:33:47 +0530

    Fixed imports

--- src/app/features/features.module.ts
+++ src/app/features/features.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
