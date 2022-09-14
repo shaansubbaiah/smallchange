4715b7fe5e9eed15b10a94216632845f817e6064
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-09-14 15:58:16 +0530

    Add MF portfolio table

--- src/app/features/portfolio/mf-table/mf-table.component.ts
+++ src/app/features/portfolio/mf-table/mf-table.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild } from '@angular/core';
+import { DataService } from 'src/app/core/services/data.service';
+
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { MfHolding } from '../../../core/models/mf-holding';
 
 @Component({
   selector: 'app-mf-table',
   templateUrl: './mf-table.component.html',
-  styleUrls: ['./mf-table.component.scss']
+  styleUrls: ['./mf-table.component.scss'],
 })
-export class MfTableComponent implements OnInit {
+export class MfTableComponent implements AfterViewInit {
+  mfs: MfHolding[] = [];
+  dataSource!: MatTableDataSource<MfHolding>;
 
-  constructor() { }
+  displayedColumns: string[] = [
+    'name',
+    'code',
+    'buy_price',
+    'LTP',
+    'asset_class',
+  ];
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService) {}
+
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.mfs = this.dataService.getMFHolding();
+    this.dataSource = new MatTableDataSource<MfHolding>(this.mfs);
+    this.dataSource.paginator = this.paginator;
   }
-
 }
