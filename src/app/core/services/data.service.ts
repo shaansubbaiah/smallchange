8fa8a8d3628936ad824f6bccd94bbe227be47007
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-09-20 13:11:31 +0530

    Refactored portfolio component

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -5,12 +5,23 @@ import {
   dummy_data_order,
   dummy_data_stocks,
 } from '../../core/models/mock-data';
+import { UserPortfolio } from '../models/user-portfolio';
 
 @Injectable({
   providedIn: 'root',
 })
 export class DataService {
-  constructor() {}
+
+  getPortfolio(): UserPortfolio {
+    return new UserPortfolio(this.getStockHolding(), this.getBondHolding(), this.getMFHolding());
+  }
+
+  private userId : string;
+
+  constructor() {
+    console.info('Data access service created!');
+    this.userId = '';
+  }
 
   getTradeHistory() {
     return dummy_data_order;
--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { BondHolding } from 'src/app/core/models/bond-holding';
+import { MfHolding } from 'src/app/core/models/mf-holding';
+import { StockHolding } from 'src/app/core/models/stock-holding';
+import { UserPortfolio } from 'src/app/core/models/user-portfolio';
+import { DataService } from 'src/app/core/services/data.service';
+import { CommonUtils } from 'src/app/utils';
 
 @Component({
   selector: 'app-portfolio',
@@ -6,7 +12,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent implements OnInit {
-  constructor() {}
 
-  ngOnInit(): void {}
+  public userPortfolio!: UserPortfolio;
+
+  private dataService: DataService = new DataService;
+
+  public getUserPortfolio(): UserPortfolio {
+    return this.dataService.getPortfolio();
+  }
+
+  ngOnInit(): void {
+    this.userPortfolio = this.getUserPortfolio();
+  }
 }
