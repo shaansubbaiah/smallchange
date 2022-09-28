19d091313e13d61bcbfe83ac6dfee330e2623cbf
preetesque <53467505+preetesque@users.noreply.github.com>
2022-09-28 09:34:07 +0530

    portfolio table basic testing

--- src/app/features/portfolio/mf-table/mf-table.component.spec.ts
+++ src/app/features/portfolio/mf-table/mf-table.component.spec.ts
@@ -1,10 +1,52 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MfHolding } from '../../../core/models/mf-holding';
 
 import { MfTableComponent } from './mf-table.component';
 
 describe('MfTableComponent', () => {
   let component: MfTableComponent;
   let fixture: ComponentFixture<MfTableComponent>;
+
+  describe('when MF Holding returns a non-empty list', () => {
+    const mockStock: MfHolding[] =  [{
+      name: 'Vanguard 500',
+      code: 'VFIAX',
+      quantity: 100,
+      buy_price: 400.34,
+      LTP: 419.68,
+    },
+    {
+      name: 'Fidelity 500 Index fund',
+      code: 'FXAIX',
+      quantity: 20,
+      buy_price: 160.45,
+      LTP: 157.76,
+    },
+    {
+      name: 'SPDR S&P 500 ETF',
+      code: 'SPY',
+      quantity: 12,
+      buy_price: 439.45,
+      LTP: 452.77,
+    }];
+// Note that the argument to configureMockStockService() is an Observable 
+// instead of a list. This allows other specs to pass the Observable
+// that is returned by throwError() so we can test error handling.
+
+    it(`will contain a table and has a "no Mutual Fund" message and 
+        will not contain data error message`, async () => {
+
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        const table = compiled.querySelector('app-reusable-table');
+
+        expect(table.rows.length).toBe(4);
+        expect(table.rows[0].cells[0].textContent).toBe('name');
+        expect(table.rows[1].cells[0].textContent).toBe('Vanguard 500');
+        expect(table.rows[2].cells[0].textContent).toBe('Fidelity 500 Index fund');
+        expect(table.rows[3].cells[0].textContent).toBe('SPDR S&P 500 ETF');
+    });
+});
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
--- src/app/features/portfolio/stock-table/stock-table.component.spec.ts
+++ src/app/features/portfolio/stock-table/stock-table.component.spec.ts
@@ -1,11 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { StockTableComponent } from './stock-table.component';
+import { StockHolding } from 'src/app/core/models/stock-holding';
 
 describe('StockTableComponent', () => {
   let component: StockTableComponent;
   let fixture: ComponentFixture<StockTableComponent>;
 
+  describe('when Stock Holding returns a non-empty list', () => {
+    const mockStock: StockHolding[] =  [{
+      name: 'Apple Inc',
+      code: 'AAPL',
+      quantity: 100,
+      buy_price: 155.34,
+      LTP: 154.29,
+      asset_class: 'Main index stocks',
+    },
+    {
+      name: 'Tesla Inc',
+      code: 'TSLA',
+      quantity: 20,
+      buy_price: 657.65,
+      LTP: 733.8,
+      asset_class: 'Main index stocks',
+    },
+    {
+      name: 'Amazon.com Inc',
+      code: 'AMZN',
+      quantity: 12,
+      buy_price: 2354.34,
+      LTP: 3471.2,
+      asset_class: 'Main index stocks',
+    }];
+// Note that the argument to configureMockStockService() is an Observable 
+// instead of a list. This allows other specs to pass the Observable
+// that is returned by throwError() so we can test error handling.
+
+    it(`will contain a table and has a "no Stock" message and 
+        will not contain data error message`, async () => {
+
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        const table = compiled.querySelector('app-reusable-table');
+
+        expect(table.rows[0].cells[0].textContent).toBe('name');
+        expect(table.rows[1].cells[0].textContent).toBe('Apple Inc');
+        expect(table.rows[2].cells[0].textContent).toBe('Tesla Inc');
+        expect(table.rows[3].cells[0].textContent).toBe('Amazon.com Inc');
+    });
+});
+ 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ StockTableComponent ]
