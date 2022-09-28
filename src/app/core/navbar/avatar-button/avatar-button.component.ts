309766875df4d705e03d549c505a31a4b64ed79a
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-09-28 11:00:13 +0530

    Fix navbar profile button warning

--- src/app/core/navbar/avatar-button/avatar-button.component.ts
+++ src/app/core/navbar/avatar-button/avatar-button.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-avatar-button',
   templateUrl: './avatar-button.component.html',
-  styleUrls: ['./avatar-button.component.scss']
+  styleUrls: ['./avatar-button.component.scss'],
 })
 export class AvatarButtonComponent implements OnInit {
+  @Input() buttonText: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
