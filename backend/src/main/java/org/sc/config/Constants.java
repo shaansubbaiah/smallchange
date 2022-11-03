8c9bb03765389f06c322791ea1e7bc68858b0595
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-11-03 17:49:36 +0530

    Initial jhipster setup

---
+++ backend/src/main/java/org/sc/config/Constants.java
@@ -0,0 +1,11 @@
+package org.sc.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM = "system";
+
+    private Constants() {}
+}
---
+++ backend/src/test/java/org/sc/IntegrationTest.java
@@ -0,0 +1,22 @@
+package org.sc;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.sc.BackendApp;
+import org.sc.config.AsyncSyncConfiguration;
+import org.sc.config.EmbeddedSQL;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { BackendApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
