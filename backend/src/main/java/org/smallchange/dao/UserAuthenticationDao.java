d3cb1fab33b77c0e14788a7c2590477e0cdc959d
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-11-03 17:14:25 +0530

    Remove old backend

--- backend/src/main/java/org/smallchange/dao/UserAuthenticationDao.java
+++
@@ -1,9 +0,0 @@
-package org.smallchange.dao;
-
-import org.smallchange.functionality.UserDetail;
-import org.smallchange.model.User;
-
-public interface UserAuthenticationDao {
-    public UserDetail loginAuthenticationService(String email, String password);
-    public UserDetail registrationAuthenticationService(String name,String email,String password);
-}
