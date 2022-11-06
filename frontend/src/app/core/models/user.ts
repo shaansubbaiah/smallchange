f7111fb11355fac1baecc4f97000229dac513753
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-11-06 23:29:17 +0530

    Refactored frontend model

--- frontend/src/app/core/models/user.ts
+++ frontend/src/app/core/models/user.ts
@@ -5,28 +5,22 @@ export class User {
   firstName: string;
   lastName: string;
   email: string;
-  passwordHash: string;
   lastLogin: number; //last successful login timestamp; to be updated in login screen
-  bankAccounts: BankAccount[];
-  token: string;
+  jwt: string;
 
   constructor(
     userName: string,
     firstName: string,
     lastName: string,
     email: string,
-    passwordHash: string,
     lastLogin: number,
-    bankAccounts: BankAccount[],
     token: string
   ) {
     this.userName = userName.trim();
     this.firstName = firstName.trim();
     this.lastName = lastName.trim();
     this.email = email.trim();
-    this.passwordHash = passwordHash.trim();
     this.lastLogin = lastLogin;
-    this.bankAccounts = bankAccounts;
-    this.token = token.trim();
+    this.jwt = token.trim();
   }
 }
