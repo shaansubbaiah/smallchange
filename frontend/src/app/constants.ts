788068e89dead15f83a7f3aac45f2398a8e2f973
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-11-07 09:09:12 +0530

    Frontend support for logout

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -1,5 +1,7 @@
 
 export class Constants {
   public static readonly BACKEND_URL = "http://localhost:8080/api";
-  public static readonly AUTH_ENDPOINT = this.BACKEND_URL + '/authenticate';
+  public static readonly AUTH_ENDPOINT = this.BACKEND_URL + '/user/authenticate';
+  public static readonly REGISTER_ENDPOINT = this.BACKEND_URL + '/user/register';
+  public static readonly LOGOUT_ENDPOINT = this.BACKEND_URL + '/user/logout';
 }
