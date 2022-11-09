c94aad72b8ab094f5b1687c4904bd79086941b7c
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-11-09 21:56:32 +0530

    Experimental homepage lesgoo

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ResolveEnd } from '@angular/router';
 import { AuthService } from './core/services/auth.service';
 
 @Component({
@@ -8,9 +9,20 @@ import { AuthService } from './core/services/auth.service';
 })
 export class AppComponent implements OnInit {
   title = 'SmallChange';
-  showUserMenu : boolean = false;
+  showUserMenu: boolean = false;
+  isHomepage = false;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {
+    this.router.events.subscribe((routerData) => {
+      if (routerData instanceof ResolveEnd) {
+        if (routerData.url === '/home') {
+          this.isHomepage = true;
+        } else {
+          this.isHomepage = false;
+        }
+      }
+    });
+  }
 
   ngOnInit() {
     this.authService.setInitialLoginStatus();
