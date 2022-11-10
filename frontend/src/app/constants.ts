efa1bec6638a930ad9131d6a09db395ce4aee1a6
Sandeep S <58328690+Sandeep-Satheesh@users.noreply.github.com>
2022-11-10 08:50:46 +0530

    Changed trade history endpoint route

--- frontend/src/app/constants.ts
+++ frontend/src/app/constants.ts
@@ -4,7 +4,7 @@ export class Constants {
   public static readonly AUTH_ENDPOINT = this.BACKEND_URL + '/user/authenticate';
   public static readonly REGISTER_ENDPOINT = this.BACKEND_URL + '/user/register';
 
-  public static readonly TRADE_HISTORY_ENDPOINT = this.BACKEND_URL + '/tradehistory';
+  public static readonly TRADE_HISTORY_ENDPOINT = this.BACKEND_URL + '/user/trade-history';
   // public static readonly STOCK_HOLDINGS_ENDPOINT = this.BACKEND_URL + '/holdings/stocks';
   // public static readonly MF_HOLDINGS_ENDPOINT = this.BACKEND_URL + '/holdings/mutual-funds';
   // public static readonly BOND_HOLDINGS_ENDPOINT = this.BACKEND_URL + '/holdings/bonds';
